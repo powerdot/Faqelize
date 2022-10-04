37468b35d30a859ccfa2f06a78fc3479e3ddf57e
powerdot <donpsix@gmail.com>
2022-10-04 23:59:42 +0500

    pwa manifest update

--- vite.config.ts
+++ vite.config.ts
@@ -20,7 +20,31 @@ export default defineConfig({
       ),
       runtimeOnly: false,
     }),
-    VitePWA({ registerType: "autoUpdate" }),
+    VitePWA({
+      registerType: "autoUpdate",
+      manifest: {
+        name: faqelize.title,
+        themeColor: "#FFFFFF",
+        msTileColor: "#FFFFFF",
+        appleMobileWebAppCapable: "yes",
+        appleMobileWebAppStatusBarStyle: "default",
+        icons: {
+          favicon32: "./img/icons/favicon-32x32.png",
+          favicon16: "./img/icons/favicon-16x16.png",
+          appleTouchIcon: "./img/icons/apple-touch-icon.png",
+          maskIcon: "./img/icons/safari-pinned-tab.svg",
+          msTileImage: "./img/icons/mstile-150x150.png",
+        },
+        manifestOptions: {
+          background_color: "#FFFFFF",
+          display: "standalone",
+          name: faqelize.title,
+          short_name: faqelize.title,
+          start_url: "./",
+          theme_color: "#FFFFFF",
+        },
+      },
+    }),
   ],
   resolve: {
     alias: {
