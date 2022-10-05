99506f510e59e36835213e00663b0ae22b069b6b
powerdot <donpsix@gmail.com>
2022-10-05 16:56:54 +0500

    build update

--- docs/ExampleAnswerPage-1664909128299.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,o as c,c as o,a as s}from"./index-1664909128299.js";const t={},n=s("h1",null,"hi",-1),a=[n];function r(_,l){return c(),o("div",null,a)}const i=e(t,[["render",r]]);export{i as default};
